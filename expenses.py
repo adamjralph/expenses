e8b9197d90804be5e730517b9dac1c6ffe8709c4
Adam Ralph <adamralph@posteo.net>
2020-10-26 20:50:01 +1100

    Started creating Expense class

--- expenses.py
+++ expenses.py
@@ -19,4 +19,31 @@
 # The program is able to exept an income and set a budget
 # based on total expenditure or by seting goals in particular categories
 
+class Expense:
+    
+    def __init__(self, name, price, category, date):
 
+        self.name = name
+        self.price = price
+        self.category = category
+        self.date = date
+
+    def new():
+
+        name = input('Please enter item name: ')
+        price = input('Please enter price: ')
+        category = input('Please enter category: ')
+        date = input('Please enter date: ')
+
+    def __str__(self):
+        
+        print(f'Item: {self.name}\nDebit: {self.price}\nCategory: {self.category}\nDate: {self.date}')
+
+# item1 = Expense('Electricity', '100', 'Bill', '01/01/2020')
+
+# print(item1)
+# new_item = Expense()
+
+expense1 = Expense.new()
+
+print(expense1)
