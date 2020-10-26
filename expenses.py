d7fec49243a2474a9a5754a25a4b65bb46a93346
Adam Ralph <adamralph@posteo.net>
2020-10-26 20:09:32 +1100

    Initial commit

---
+++ expenses.py
@@ -0,0 +1,22 @@
+#
+# A program which enable the user to enter data to track expenses
+# 
+# An expense includes the following properties
+#
+# - Name
+# - Price
+# - Quantity
+# - Category
+# - Date
+#
+# The program is able to search the expenses and return various information
+# such as:
+#
+# - Total expenses in a given time period
+# - Total expenses for a given category
+# - 
+# 
+# The program is able to exept an income and set a budget
+# based on total expenditure or by seting goals in particular categories
+
+
