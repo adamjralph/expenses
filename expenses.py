7a35facca7ee5e246ccb5bc9503a7a2fe368745e
Adam Ralph <adamralph@posteo.net>
2020-11-02 19:55:39 +1100

    Reads-updates-writes-working!

--- expenses.py
+++ expenses.py
@@ -1,30 +1,10 @@
-
-# A program which enable the user to enter data to track expenses
-#
-# An expense includes the following properties
-#
-# - Name
-# - Price
-# - Quantity
-# - Category
-# - Date
-#
-# The program is able to search the expenses and return various information
-# such as:
-#
-# - Total expenses in a given time period
-# - Total expenses for a given category
-# -
-#
-# The program is able to exept an income and set a budget
-# based on total expenditure or by seting goals in particular categories
 from datetime import date
 import ast
 
-def create_id(data_dict):
+def create_id(data):
     id_num = 0
-    if data_dict:
-        max_id = max(k for k, v in data_dict.items())
+    if data:
+        max_id = max(k for k, v in data.items())
         print(max_id)
         id_num = max_id
         return id_num
@@ -46,54 +26,51 @@ def add_category():
 def add_date():
     return date.today().strftime('%Y-%m-%d')
 
-data_dict = {}
+# data_dict = {}
 
-def read_data(file_written):
+def read_data():
 
-    if file_written == True:
+    try:
         with open('expense_data.txt', 'r') as f:
             data = f.read()
         data_dict = ast.literal_eval(data)
         print(data_dict)
         print(type(data_dict))
         return data_dict
-    else:
-        print('Empty')
+    except SyntaxError:
+        print('Dictionary is empty!')
+        data_dict = {}
+        print(data_dict)
+        print(type(data_dict))
+        return data_dict
 
-def create_entry(create_id, data_dict):
+def create_entry(data):
     
-    new_id = create_id(data_dict)
+    new_id = create_id(data)
     entry_list = [add_item(), add_price(), add_category(), add_date()]
-    data_dict[new_id + 1] = entry_list
+    data[new_id + 1] = entry_list
     
-file_written = False
 def write_dict(data_dict):
 
-    data_file = open('expense_data.txt', 'a')
+    data_file = open('expense_data.txt', 'w')
     data_file.write(str(data_dict))
-    file_written = True
     print('Data written to file.')
     data_file.close()
-    return file_written
 
-        
-
-def session_on(create_id, data_dict):
-
-    new_id = create_id(data_dict)
+def session_on(data):
+    
     while True:
 
         session = input("To enter a new expense please type 'e' or to quit type 'q': ")
         if session == 'q':
-            write_dict(data_dict)
+            write_dict(data)
             print('Session ended. Goodbye!')
             break
         else:
-            create_entry(create_id, data_dict)
-            #create_id(data_dict)
-            read_data(file_written)
-read_data(file_written)
-session_on(create_id, data_dict)
+            create_entry(data)
+
+data = read_data()
+session_on(data)
 
 # def store_date(create):
 
